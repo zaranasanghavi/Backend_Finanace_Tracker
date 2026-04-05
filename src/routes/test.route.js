6ddf9854287070389121eebf315f872e0b41c8ef
Sanghavi Zarana Naresh <157245898+zaranasanghavi@users.noreply.github.com>
2026-04-06 01:12:07 +0530

    Remove unused middleware routes
    Removed commented-out authentication and RBAC middleware routes.

--- src/routes/test.route.js
+++ src/routes/test.route.js
@@ -12,25 +12,5 @@ router.get(
   });
  }
 );
-/*
-const authMiddleware = require("../src/middlewares/auth.middleware");
 
-app.get("/api/test", authMiddleware, (req, res) => {
-  res.json({
-    message: "Protected route working",
-    user: req.user
-  });
-});
-
-const rbac = require("../src/middlewares/rbac.middleware.js");
-
-app.get(
- "/admin-test",
- authMiddleware,
- rbac("admin"),
- (req,res)=>{
-  res.json({ message: "Admin only" });
- }
-);
-*/
 module.exports = router;
